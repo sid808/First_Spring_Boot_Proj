83af070c10a20efe84c830bf09ac21db3439786f
Licious <Licious@Admins-MacBook-Pro.local>
2021-09-30 05:29:18 +0530

    Making Spring Bean and using @Component

--- src/main/java/com/example/demo/FirstProjApplication.java
+++ src/main/java/com/example/demo/FirstProjApplication.java
@@ -2,14 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstProjApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstProjApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstProjApplication.class, args);
 		
 		
 		System.out.println("Welcome to Boot...");
+		
+		Alien a = context.getBean(Alien.class);
+		
+		a.show();
 	}
 }
